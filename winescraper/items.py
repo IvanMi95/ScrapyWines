d7373798641f8446b6f8dd62bcb208c4e6dcc438
ivan milanovic <ivanmilanovic1995@gmail.com>
2024-06-06 22:05:09 +0200

    feat: added items

---
+++ winescraper/items.py
@@ -0,0 +1,25 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class WineItem(scrapy.Item):
+    name = scrapy.Field()
+    url = scrapy.Field()
+    sale_price = scrapy.Field()
+    original_price = scrapy.Field()
+    lowest_price = scrapy.Field()
+    awards = scrapy.Field()
+    discount_percentage = scrapy.Field()
+    availability = scrapy.Field()
+    appellation = scrapy.Field()
+    grape_variety = scrapy.Field()
+    alcohol_content = scrapy.Field()
+    serving_temperature = scrapy.Field()
+    wine_type = scrapy.Field()
+    vivino_rating = scrapy.Field()
+    vivino_reviews = scrapy.Field()
+    vivino_link = scrapy.Field()

0c5ee64e905badc12143c67faabc66199334e461
Ivan Milanovic <ivanmilanovic1995@gmail.com>
2024-06-09 21:55:48 +0200

    feat: added sqlAlchemy methods

--- project/database.py
+++ project/database.py
@@ -1,12 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import QueuePool, create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session, Session, declarative_base
 
 from project.config import settings
 
 # https://fastapi.tiangolo.com/tutorial/sql-databases/#create-the-sqlalchemy-engine
 engine = create_engine(
-    settings.DATABASE_URL, connect_args=settings.DATABASE_CONNECT_DICT
+    settings.DATABASE_URL,
+    poolclass=QueuePool,
+    pool_size=40,
+    max_overflow=5,
+    pool_timeout=30,
+    pool_recycle=3600,
+    echo=True
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_session() -> Session:
+    session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    custom_scoped_session = scoped_session(session_local)
+    return custom_scoped_session()
+
 
 Base = declarative_base()

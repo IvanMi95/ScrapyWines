5918c31d472169eb594ee3c630320bedddcad4c5
Ivan Milanovic <ivanmilanovic1995@gmail.com>
2024-06-09 16:05:08 +0200

    feat: moved files

--- project/__init__.py
+++ project/__init__.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
 
-from project.wines import wine_router                # new
+from project.celery_utils import create_celery
+from project.wines import wine_router
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
+    app.celery_app = create_celery()
+
     app.include_router(wine_router)
 
     @app.get("/")
     async def root():
         return {"message": "Hello World"}
-
     return app
